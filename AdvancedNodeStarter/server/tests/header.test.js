e4da156fdeafee368aaae491a11e0cbbac89be83
richard munoz <richosojason@msn.com>
2021-01-12 17:54:00 -0500

    added new test in header

--- AdvancedNodeStarter/server/tests/header.test.js
+++ AdvancedNodeStarter/server/tests/header.test.js
@@ -10,11 +10,18 @@ beforeEach(async () => {
   await page.goto("localhost:3000");
 });
 
-afterEach(async ()=>{
+afterEach(async () => {
   await browser.close();
 });
 
-test("we can launch a browser", async () => {
+test("the header has the correct test", async () => {
   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
   expect(text).toEqual("Blogster");
+});
+
+test("clicking login starts oauth flow", async () => {
+  await page.click(".right a");
+
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
 });
