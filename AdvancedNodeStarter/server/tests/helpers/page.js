87cc1d7647308791e0e3b532f6ca20aba28a2d1c
richard munoz <richosojason@msn.com>
2021-01-24 15:10:17 -0500

    added login function to page class

--- AdvancedNodeStarter/server/tests/helpers/page.js
+++ AdvancedNodeStarter/server/tests/helpers/page.js
@@ -1,4 +1,6 @@
 const puppeteer = require("puppeteer");
+const sessionFactory = require("../factories/sessionFactory");
+const userFactory = require("../factories/userFactory");
 
 class CustomPage {
   static async build() {
@@ -19,6 +21,22 @@ class CustomPage {
 
   constructor(page) {
     this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+
+    await this.page.setCookie({ name: "session", value: session });
+    await this.page.setCookie({ name: "session.sig", value: sig });
+    await this.page.goto("localhost:3000");
+
+    // wait for this selector to then proceed to next statement
+    await this.page.waitFor('a[href="/auth/logout"]');
+  }
+
+  async getContentsOf(selector) {
+    return this.page.$eval(selector, (el) => el.innerHTML);
   }
 }
 
