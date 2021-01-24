80c8a2ed7baebb2ae0d002c9d85931ee07d35a14
richard munoz <richosojason@msn.com>
2021-01-24 15:10:32 -0500

    refactored header test

--- AdvancedNodeStarter/server/tests/header.test.js
+++ AdvancedNodeStarter/server/tests/header.test.js
@@ -1,5 +1,3 @@
-const sessionFactory = require("./factories/sessionFactory");
-const userFactory = require("./factories/userFactory");
 const Page = require("./helpers/page");
 
 let page;
@@ -14,7 +12,7 @@ afterEach(async () => {
 });
 
 test("the header has the correct test", async () => {
-  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+  const text = await page.getContentsOf("a.brand-logo");
   expect(text).toEqual("Blogster");
 });
 
@@ -26,15 +24,7 @@ test("clicking login starts oauth flow", async () => {
 });
 
 test("when signed in, shows logout button", async () => {
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-
-  await page.setCookie({ name: "session", value: session });
-  await page.setCookie({ name: "session.sig", value: sig });
-  await page.goto("localhost:3000");
-
-  // wait for this selector to then proceed to next statement
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login();
 
   const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
   expect(text).toEqual("Logout");
