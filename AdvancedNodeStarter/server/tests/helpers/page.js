9414c80dc630b06bc3b95821ba2c5983f97db713
richard munoz <richosojason@msn.com>
2021-01-23 21:20:57 -0500

    added proxy page

---
+++ AdvancedNodeStarter/server/tests/helpers/page.js
@@ -0,0 +1,25 @@
+const puppeteer = require("puppeteer");
+
+class CustomPage {
+  static async build() {
+    //  will generate a new puppeteer page class
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+
+    return new Proxy(customPage, {
+      get: function (target, property) {
+        return target[property] || page[property] || browser[property];
+      },
+    });
+  }
+
+  constructor(page) {
+    this.page = page;
+  }
+}
+
+module.exports = CustomPage;
