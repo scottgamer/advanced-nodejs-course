648c272630db9c99755923658983151b67f3801e
richard munoz <richosojason@msn.com>
2021-01-25 21:41:05 -0500

    added describe funcitons to wrap tests

--- AdvancedNodeStarter/server/tests/blogs.test.js
+++ AdvancedNodeStarter/server/tests/blogs.test.js
@@ -11,10 +11,52 @@ afterEach(async () => {
   await page.close();
 });
 
-test("when logged in, can see blog create form", async () => {
-  await page.login();
-  await page.click("a.btn-floating");
+describe("When logged in", async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click("a.btn-floating");
+  });
 
-  const label = await page.getContentsOf("form label");
-  expect(label).toEqual("Blog Title");
+  test("can see blog create form", async () => {
+    const label = await page.getContentsOf("form label");
+    expect(label).toEqual("Blog Title");
+  });
+
+  describe("and using valid inputs", async () => {
+    beforeEach(async () => {
+      await page.type(".title input", "My title");
+      await page.type(".content input", "My content");
+      await page.click("form button");
+    });
+
+    test("submitting takes user to review screen", async () => {
+      const text = await page.getContentsOf("h5");
+      expect(text).toEqual("Please confirm your entries");
+    });
+
+    test("submitting then saving adds blog to index page", async () => {
+      await page.click("button.green");
+      await page.waitFor(".card");
+
+      const title = await page.getContentsOf(".card-title");
+      const content = await page.getContentsOf("p");
+
+      expect(title).toEqual("My title");
+      expect(content).toEqual("My content");
+    });
+  });
+
+  describe("and using invalid inputs", async () => {
+    beforeEach(async () => {
+      await page.click("form button");
+    });
+
+    test("the form shows an error message", async () => {
+      const titleError = await page.getContentsOf(".title .red-text");
+      const contentError = await page.getContentsOf(".content .red-text");
+
+      expect(titleError).toEqual("You must provide a value");
+      expect(contentError).toEqual("You must provide a value");
+    });
+  });
 });
